ce7512fb484890eb75e9e2bd1c59cd31f1714dcb
Xalilov <eljjey@gmail.com>
2021-02-07 17:15:27 +0500

    send photo iz fayl

--- sendphoto.py
+++ sendphoto.py
@@ -2,14 +2,15 @@ import requests
 from pprint import pprint
 
 token = "1679454024:AAEs98zOJvEYgdYwbZgFaJQQMSsBHFWHdnc"
-def sendPhoto(ids):
+def sendPhoto(idx):
     url = f'https://api.telegram.org/bot{token}/sendPhoto'
+    photo=open('photo.jpg','rb')
     payload ={
-        'chat_id':ids,
-        'photo':'AgACAgQAAxkDAAMQYB_WjPIennkmT1SxPc8oaFSooDkAAgGsMRt_XgRRWt0yqhJxhTskifkoXQADAQADAgADbQADLXQDAAEeBA',
-        'caption':'TITLE'
+        'chat_id':idx,
+        # 'photo':'AgACAgQAAxkDAAMQYB_WjPIennkmT1SxPc8oaFSooDkAAgGsMRt_XgRRWt0yqhJxhTskifkoXQADAQADAgADbQADLXQDAAEeBA',
+        'caption':"Oq ko'ylak"
     }
-    r=requests.get(url,params=payload)
+    r=requests.post(url,params=payload,files={'photo':photo})
     
     return r.json()
 
